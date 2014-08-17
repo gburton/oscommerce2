37c2661f292120a7558b011a548b5a9a70736e00
acidvertigo <ldanzelmo@gmail.com>
2014-08-17 11:56:39 +0200

    Unify template top with header.php

--- catalog/includes/template_top.php
+++ catalog/includes/template_top.php
@@ -20,6 +20,7 @@
     $oscTemplate->setGridContentWidth($oscTemplate->getGridContentWidth() + $oscTemplate->getGridColumnWidth());
   }
 ?>
+
 <!DOCTYPE html>
 <html <?php echo HTML_PARAMS; ?>>
 <head>
@@ -54,7 +55,56 @@
 
 <div class="row">
 
-<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<?php
 
+  if ($messageStack->size('header') > 0) {
+    echo '<div>' . $messageStack->output('header') . '</div>';
+  }
+?>
+
+  <div id="storeLogo" class="col-sm-6"><?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT) . '">' . tep_image(DIR_WS_IMAGES . 'store_logo.png', STORE_NAME) . '</a>'; ?></div>
+
+  <div id="headerShortcuts" class="col-sm-6 text-right">
+    <div class="btn-group">
+<?php
+  echo tep_draw_button(HEADER_TITLE_CART_CONTENTS . ($_SESSION['cart']->count_contents() > 0 ? ' (' . $_SESSION['cart']->count_contents() . ')' : ''), 'glyphicon glyphicon-shopping-cart', tep_href_link(FILENAME_SHOPPING_CART)) .
+       tep_draw_button(HEADER_TITLE_CHECKOUT, 'glyphicon glyphicon-credit-card', tep_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL')) .
+       tep_draw_button(HEADER_TITLE_MY_ACCOUNT, 'glyphicon glyphicon-user', tep_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+
+  if (isset($_SESSION['customer_id'])) {
+    echo tep_draw_button(HEADER_TITLE_LOGOFF, 'glyphicon glyphicon-log-out', tep_href_link(FILENAME_LOGOFF, '', 'SSL'));
+  }
+?>
+    </div>
+  </div>
+
+<div class="clearfix"></div>
+<div class="col-xs-12"><?php echo $breadcrumb->trail(' &raquo; '); ?></div>
+
+<?php
+  if (isset($_GET['error_message']) && tep_not_null($_GET['error_message'])) {
+?>
+<div class="clearfix"></div>
+<div class="col-xs-12">
+  <div class="alert alert-danger">
+    <a href="#" class="close glyphicon glyphicon-remove" data-dismiss="alert"></a>
+    <?php echo htmlspecialchars(stripslashes(urldecode($_GET['error_message']))); ?>
+  </div>
+</div>
+<?php
+  }
+
+  if (isset($_GET['info_message']) && tep_not_null($_GET['info_message'])) {
+?>
+<div class="clearfix"></div>
+<div class="col-xs-12">
+  <div class="alert alert-info">
+    <a href="#" class="close glyphicon glyphicon-remove" data-dismiss="alert"></a>
+    <?php echo htmlspecialchars(stripslashes(urldecode($_GET['info_message']))); ?>
+  </div>
+</div>
+<?php
+  }
+?>
 
 <div id="bodyContent" class="col-md-<?php echo $oscTemplate->getGridContentWidth(); ?> <?php echo ($oscTemplate->hasBlocks('boxes_column_left') ? 'col-md-push-' . $oscTemplate->getGridColumnWidth() : ''); ?>">
