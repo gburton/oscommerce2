d75cdd416f993359d563bc253d8c1bb739cfec79
acidvertigo <ldanzelmo@gmail.com>
2014-08-29 19:43:13 +0200

    Add reviews class

---
+++ catalog/includes/classes/reviews.php
@@ -0,0 +1,19 @@
+<?php
+
+class reviews
+ 
+{
+  protected $products_options_name_arr;
+  protected $products_options_array;
+  protected $selected_attribute;
+ 
+  public function __construct($products_id) {
+   $reviews_query = tep_db_query("select count(*) as count from " . TABLE_REVIEWS . " r, " . TABLE_REVIEWS_DESCRIPTION . " rd where r.products_id = '" . (int)$_GET['products_id'] . "' and r.reviews_id = rd.reviews_id and rd.languages_id = '" . (int)$_SESSION['languages_id'] . "' and reviews_status = 1");
+    
+    $this->reviews = tep_db_fetch_array($reviews_query);
+  }
+  
+  public function getReviews() {
+  return $this->reviews;
+  }
+}
