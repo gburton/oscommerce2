b68fdde244151abba0e5b0e732c525b676d6c2f8
acidvertigo <ldanzelmo@gmail.com>
2014-09-21 13:59:49 +0200

    Fix product price output

--- catalog/includes/modules/pages/tp_product.php
+++ catalog/includes/modules/pages/tp_product.php
@@ -20,20 +20,32 @@
     }
 
     function build() {
-      global $oscTemplate;
+      global $oscTemplate, $currencies;
 
       $output = '';
 
       $output .= tep_draw_form('cart_quantity', tep_href_link(FILENAME_PRODUCT_INFO, tep_get_all_get_params(array('action')) . 'action=add_product'), 'post', 'class="form-horizontal" role="form"');
       
-      $output .=   '<div class="page-header">' .
-                   '  <h1 class="pull-right">' . $this->data['products_price'] . '</h1>' .
-                   '  <h1>' . $this->data['products_name'] . '</h1>';
+      // check for special price otherwise will return the list price
+    
+      
+      $output .=   '<div class="page-header">';
+      $output .=   '  <h1 class="pull-right">';
+      if ($new_price = tep_get_products_special_price($this->data['products_id'])) {
+      $output .= '<del>' . $currencies->display_price($this->data['products_price'], tep_get_tax_rate($this->data['products_tax_class_id'])) . '</del> <span class="productSpecialPrice">' . $currencies->display_price($new_price, tep_get_tax_rate($this->data['products_tax_class_id'])) . '</span>';
+      } else {
+        $output .= $currencies->display_price($this->data['products_price'], tep_get_tax_rate($this->data['products_tax_class_id']));
+      }
+      
+      $output .=  '</h1>';
+      $output .=   '  <h1>' . $this->data['products_name'] . '</h1>';
                    
       if ( $this->data['products_model'] ) {
       $output .= '<br />' .
                  '<span class="smallText">[' . $this->data['products_model'] . ']</span>'; 
       }
+      
+      
         
       $output .= '</div>';
                       
