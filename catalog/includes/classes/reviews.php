c272c4520b10ff23b3c730f727b593d32f92a822
acidvertigo <ldanzelmo@gmail.com>
2014-09-21 11:55:47 +0200

    Apply class changes to review pages

--- catalog/includes/classes/reviews.php
+++ catalog/includes/classes/reviews.php
@@ -16,18 +16,36 @@ class reviews
 
   public function __construct($products_id) 
   {
-   
+   $this->id = $products_id;
   }
   
-  public function getReviews()
+  public function getReviewsraw()
   {
     $reviews_query_raw = "select r.reviews_id, left(rd.reviews_text, 100) as reviews_text, r.reviews_rating, r.date_added, p.products_id, pd.products_name, p.products_image, r.customers_name from " . TABLE_REVIEWS . " r, " . TABLE_REVIEWS_DESCRIPTION . " rd, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where p.products_status = '1' and p.products_id = r.products_id and r.reviews_id = rd.reviews_id and p.products_id = pd.products_id and pd.language_id = '" . (int)$_SESSION['languages_id'] . "' and rd.languages_id = '" . (int)$_SESSION['languages_id'] . "' and reviews_status = 1 order by r.reviews_id DESC";
     return $reviews_query_raw;
   }
   
+   public function getReviews()
+  { 
+    $reviews_query_raw = "select r.reviews_id, left(rd.reviews_text, 100) as reviews_text, r.reviews_rating, r.date_added, r.customers_name from " . TABLE_REVIEWS . " r, " . TABLE_REVIEWS_DESCRIPTION . " rd where r.products_id = '" . (int)$this->id . "' and r.reviews_id = rd.reviews_id and rd.languages_id = '" . (int)$_SESSION['languages_id'] . "' and r.reviews_status = 1 order by r.reviews_id desc"; 
+    
+    return $reviews_query_raw;
+  } 
+
+   public function getReviewssplit($query)
+  {  
+    return new splitPageResults($query, MAX_DISPLAY_NEW_REVIEWS);
+  } 
+  
+   public function getReviewsavg()
+  {  
+      $average_query = tep_db_query("select AVG(r.reviews_rating) as average, COUNT(r.reviews_rating) as count from " . TABLE_REVIEWS . " r where r.products_id = '" . (int)$this->id . "' and r.reviews_status = 1");
+      return tep_db_fetch_array($average_query); 
+  } 
+  
   public function getReviewscount()
   {
-    $reviews_query = tep_db_query("select count(*) as count from " . TABLE_REVIEWS . " r, " . TABLE_REVIEWS_DESCRIPTION . " rd where r.products_id = '" . (int)$_GET['products_id'] . "' and r.reviews_id = rd.reviews_id and rd.languages_id = '" . (int)$_SESSION['languages_id'] . "' and reviews_status = 1");
+    $reviews_query = tep_db_query("select count(*) as count from " . TABLE_REVIEWS . " r, " . TABLE_REVIEWS_DESCRIPTION . " rd where r.products_id = '" .(int)$this->id . "' and r.reviews_id = rd.reviews_id and rd.languages_id = '" . (int)$_SESSION['languages_id'] . "' and reviews_status = 1");
     return tep_db_fetch_array($reviews_query); 
   }
   
--- catalog/product_reviews.php
+++ catalog/product_reviews.php
@@ -16,12 +16,7 @@
     tep_redirect(tep_href_link(FILENAME_REVIEWS));
   }
 
-  $product_info_query = tep_db_query("select p.products_id, p.products_model, p.products_image, p.products_price, p.products_tax_class_id, pd.products_name from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where p.products_id = '" . (int)$_GET['products_id'] . "' and p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '" . (int)$_SESSION['languages_id'] . "'");
-  if (!tep_db_num_rows($product_info_query)) {
-    tep_redirect(tep_href_link(FILENAME_REVIEWS));
-  } else {
-    $product_info = tep_db_fetch_array($product_info_query);
-  }
+    $product_info = $product->getData();
 
   if ($new_price = tep_get_products_special_price($product_info['products_id'])) {
     $products_price = '<del>' . $currencies->display_price($product_info['products_price'], tep_get_tax_rate($product_info['products_tax_class_id'])) . '</del> <span class="productSpecialPrice">' . $currencies->display_price($new_price, tep_get_tax_rate($product_info['products_tax_class_id'])) . '</span>';
@@ -58,8 +53,8 @@
   <div class="row">
     <div class="col-sm-8 text-center alert alert-success">
       <?php
-      $average_query = tep_db_query("select AVG(r.reviews_rating) as average, COUNT(r.reviews_rating) as count from " . TABLE_REVIEWS . " r where r.products_id = '" . (int)$product_info['products_id'] . "' and r.reviews_status = 1");
-      $average = tep_db_fetch_array($average_query);
+      
+      $average = $reviews->getReviewsavg();
 
       echo sprintf(REVIEWS_TEXT_AVERAGE, tep_output_string_protected($average['count']), tep_draw_stars(tep_output_string_protected(round($average['average']))));
       ?>
@@ -88,8 +83,7 @@
   </div>
 <?php
 
-  $reviews_query_raw = "select r.reviews_id, left(rd.reviews_text, 100) as reviews_text, r.reviews_rating, r.date_added, r.customers_name from " . TABLE_REVIEWS . " r, " . TABLE_REVIEWS_DESCRIPTION . " rd where r.products_id = '" . (int)$product_info['products_id'] . "' and r.reviews_id = rd.reviews_id and rd.languages_id = '" . (int)$_SESSION['languages_id'] . "' and r.reviews_status = 1 order by r.reviews_id desc";
-  $reviews_split = new splitPageResults($reviews_query_raw, MAX_DISPLAY_NEW_REVIEWS);
+  $reviews_split = new splitPageResults($reviews->getReviews(), MAX_DISPLAY_NEW_REVIEWS);
 
   if ($reviews_split->number_of_rows > 0) {
     if ((PREV_NEXT_BAR_LOCATION == '1') || (PREV_NEXT_BAR_LOCATION == '3')) {
