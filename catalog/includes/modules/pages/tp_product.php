69c42c0bf38633c2b42f7ccc22a8422d0ae530fb
Luca <ldanzelmo@gmail.com>
2014-09-23 18:06:11 +0200

    Better htmlcontent

--- catalog/includes/modules/pages/tp_product.php
+++ catalog/includes/modules/pages/tp_product.php
@@ -44,8 +44,8 @@
     if (tep_not_null($oscTemplate->_data[$this->group]['products_image'])) {
       $photoset_layout = '1';
  
-      $pi_query = $product->getHtmlcontent();
-      $pi_total = tep_db_num_rows($pi_query);
+      $image = $product->getHtmlcontent();
+      $pi_total = (sizeof($image));
 
       if ($pi_total > 0) {
         $pi_sub = $pi_total-1;
@@ -58,25 +58,7 @@
         if ($pi_sub > 0) {
           $photoset_layout .= ($pi_total > 5) ? 5 : $pi_sub;
         }
-
- 
-        $pi_counter = 0;
-        $image = array();
-
-         while ($pi = tep_db_fetch_array($pi_query)) {
-          $pi_counter++;
- 
-          if (tep_not_null($pi['htmlcontent'])) {
-            $image[$pi_counter]['htmlcontent'][] = $pi['htmlcontent'];
-          }
- 
-            $image[$pi_counter]['image'] = tep_image(DIR_WS_IMAGES . $pi['image'], '', '', '', 'id="piGalImg_' . $pi_counter . '"');
-         }
-
-        if ( !empty($image[$pi_counter]['htmlcontent']) ) {
-           $image[$pi_counter]['none'] = implode('', $image[$pi_counter]['htmlcontent']);
-        }
-        
+      
       } else {
         $image = tep_image(DIR_WS_IMAGES . $oscTemplate->_data[$this->group]['products_image'], addslashes($oscTemplate->_data[$this->group]['products_name']));
       }
