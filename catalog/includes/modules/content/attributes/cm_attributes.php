98171a18523d8f17330b940ee45f5fccddaed0a9
Luca <ldanzelmo@gmail.com>
2014-09-25 12:00:59 +0200

    Fix attributes installation. Enable the attributes modules by default on a new installation

--- catalog/includes/modules/content/attributes/cm_attributes.php
+++ catalog/includes/modules/content/attributes/cm_attributes.php
@@ -60,7 +60,7 @@
     function install() {
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Attributes Module', 'MODULE_CONTENT_ATTRIBUTES_STATUS', 'True', 'Do you want to enable the attribtes module?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable attributes type', 'MODULE_CONTENT_ATTRIBUTES_TYPE', 'True', 'Enable the attributes type?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_ATTRIBUTES_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_ATTRIBUTES_SORT_ORDER', '1000', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
     }
 
     function remove() {
